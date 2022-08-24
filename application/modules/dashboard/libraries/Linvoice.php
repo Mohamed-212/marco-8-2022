a1343235599553ccd012b54c854264e917b5a978
abo3adel <ahmed.adel@212solutionsllc.com>
2022-08-24 10:19:18 +0100

    redesign invoice styles

--- application/modules/dashboard/libraries/Linvoice.php
+++ application/modules/dashboard/libraries/Linvoice.php
@@ -229,6 +229,7 @@ class Linvoice {
 			'title'			   =>display('invoice_details'),
 			'invoice_id'	   =>$invoice_detail[0]['invoice_id'],
 			'invoice_no'	   =>$invoice_detail[0]['invoice'],
+			'customer_id'      => $invoice_detail[0]['customer_id'],
 			'customer_name'	   =>$invoice_detail[0]['customer_name'],
 			'customer_mobile'  =>$invoice_detail[0]['customer_mobile'],
 			'customer_email'   =>$invoice_detail[0]['customer_email'],
