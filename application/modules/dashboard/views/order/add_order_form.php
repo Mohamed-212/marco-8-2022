8a366c7f3fb8bbb7b30b4d20bfaabc435f25d774
abo3adel <ahmed.adel@212solutionsllc.com>
2022-09-11 14:28:45 +0100

    fix order discount lang key

--- application/modules/dashboard/views/order/add_order_form.php
+++ application/modules/dashboard/views/order/add_order_form.php
@@ -519,7 +519,7 @@
                                     </tr>
                                     <tr>
                                         <td class="text-right" colspan="2">
-                                            <b><?php echo display('order_percentage_discount') ?>:</b>
+                                            <b><?php echo display('order_discount_percentage') ?>:</b>
                                         </td>
                                         <td class="text-right" colspan="2">
                                             <input type="text" id="percentage_discount" class="form-control text-right" name="percentage_discount" placeholder="0 %" onkeyup="calculateSum();" onchange="calculateSum();" />
