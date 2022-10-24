358d7b6e408efe7d82e6834f87f279eadf01bc94
abo3adel <ahmed.adel@212solutionsllc.com>
2022-10-24 18:23:33 +0100

    fix show add purchase always

--- application/modules/dashboard/views/purchase/purchase.php
+++ application/modules/dashboard/views/purchase/purchase.php
@@ -1,4 +1,4 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <!-- Manage Purchase Start -->
 <div class="content-wrapper">
     <section class="content-header">
@@ -20,32 +20,29 @@
         <?php
         $message = $this->session->userdata('message');
         if (isset($message)) {
-            ?>
+        ?>
             <div class="alert alert-info alert-dismissable">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
                 <?php echo $message ?>
             </div>
-            <?php
+        <?php
             $this->session->unset_userdata('message');
         }
         $error_message = $this->session->userdata('error_message');
         if (isset($error_message)) {
-            ?>
+        ?>
             <div class="alert alert-danger alert-dismissable">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
                 <?php echo $error_message ?>
             </div>
-            <?php
+        <?php
             $this->session->unset_userdata('error_message');
         }
         ?>
         <div class="row">
             <div class="col-sm-12">
                 <div class="column">
-                    <?php if ($this->permission->check_label('add_purchase')->create()->access()) { ?>
-                        <a href="<?php echo base_url('dashboard/Cpurchase') ?>" class="btn btn-success m-b-5 m-r-2"><i
-                                class="ti-plus"> </i> <?php echo display('add_purchase') ?></a>
-                        <?php } ?>
+                    <a href="<?php echo base_url('dashboard/Cpurchase') ?>" class="btn btn-success m-b-5 m-r-2"><i class="ti-plus"> </i> <?php echo display('add_purchase') ?></a>
                 </div>
             </div>
         </div>
@@ -76,38 +73,34 @@
                                     <?php
                                     if ($purchases_list) {
                                         foreach ($purchases_list as $purchase) {
-                                            ?>
+                                    ?>
                                             <tr>
                                                 <td><?php echo $purchase['sl'] ?></td>
                                                 <td>
-                                                    <a
-                                                        href="<?php echo base_url() . 'dashboard/Cpurchase/purchase_details_data/' . $purchase['purchase_id']; ?>">
-                                                        <?php echo html_escape($purchase['invoice']) ?><i
-                                                            class="fa fa-tasks pull-right" aria-hidden="true"></i>
+                                                    <a href="<?php echo base_url() . 'dashboard/Cpurchase/purchase_details_data/' . $purchase['purchase_id']; ?>">
+                                                        <?php echo html_escape($purchase['invoice']) ?><i class="fa fa-tasks pull-right" aria-hidden="true"></i>
                                                     </a>
                                                 </td>
                                                 <td>
-                                                    <a
-                                                        href="<?php echo base_url() . 'dashboard/Csupplier/supplier_details/' . $purchase['supplier_id']; ?>">
-                                                        <?php echo html_escape($purchase['supplier_name']) ?> <i
-                                                            class="fa fa-user pull-right" aria-hidden="true"></i>
+                                                    <a href="<?php echo base_url() . 'dashboard/Csupplier/supplier_details/' . $purchase['supplier_id']; ?>">
+                                                        <?php echo html_escape($purchase['supplier_name']) ?> <i class="fa fa-user pull-right" aria-hidden="true"></i>
                                                     </a>
                                                 </td>
                                                 <td>
                                                     <?php
                                                     if (!empty($purchase['store_id'])) {
                                                         $store = $this->db->select('*')
-                                                                ->from('store_set')
-                                                                ->where('store_id', $purchase['store_id'])
-                                                                ->get()
-                                                                ->row();
+                                                            ->from('store_set')
+                                                            ->where('store_id', $purchase['store_id'])
+                                                            ->get()
+                                                            ->row();
                                                         echo html_escape($store->store_name . ' (' . display('store') . ')');
                                                     } else {
                                                         $wearhouse = $this->db->select('*')
-                                                                ->from('wearhouse_set')
-                                                                ->where('wearhouse_id', $purchase['wearhouse_id'])
-                                                                ->get()
-                                                                ->row();
+                                                            ->from('wearhouse_set')
+                                                            ->where('wearhouse_id', $purchase['wearhouse_id'])
+                                                            ->get()
+                                                            ->row();
                                                         echo html_escape($wearhouse->wearhouse_name) . ' (' . display('wearhouse') . ')';
                                                     }
                                                     ?>
@@ -117,47 +110,32 @@
                                                     <?php echo html_escape(($position == 0) ? $currency . ' ' . $purchase['grand_total_amount'] : $purchase['grand_total_amount'] . ' ' . $currency) ?>
                                                 </td>
                                                 <td>
-                                        <center>
-                                            <?php echo form_open() ?>
-                                            <?php if (isset($purchase['file']) && null !== $purchase['file']) : ?>
-                                                <a target="_blank" href="<?php echo base_url() . 'dashboard/Cpurchase/purchase_download_attachment/' . $purchase['purchase_id'] . '?file=' . $purchase['file']; ?>"
-                                            class="btn btn-success btn-sm" data-toggle="tooltip"
-                                            data-placement="left" title="<?php echo display('download_attachment') ?>"><i
-                                                class="fa fa-download" aria-hidden="true"></i></a>
-                                            <?php endif; ?>
-                                            <a href="<?php echo base_url() . 'dashboard/Cpurchase/purchase_inserted_data/' . $purchase['purchase_id']; ?>"
-                                               class="btn btn-success btn-sm" data-toggle="tooltip"
-                                               data-placement="left" title="<?php echo display('invoice') ?>"><i
-                                                    class="fa fa-window-restore" aria-hidden="true"></i></a>
-                                            <a href="<?php echo base_url() . 'dashboard/Cpurchase_return/purchase_return_form/' . $purchase['purchase_id']; ?>"
-                                               class="btn btn-warning btn-sm" data-toggle="tooltip"
-                                               data-placement="left"
-                                               title="<?php echo display('return_purchase') ?>"><i
-                                                    class="fa fa-undo" aria-hidden="true"></i></a>
-                                                <?php if ($this->permission->check_label('manage_purchase')->update()->access()) { ?>
-                                                    <!--<a href="<?php echo base_url() . 'dashboard/Cpurchase/purchase_update_form/' . $purchase['purchase_id']; ?>"
+                                                    <center>
+                                                        <?php echo form_open() ?>
+                                                        <?php if (isset($purchase['file']) && null !== $purchase['file']) : ?>
+                                                            <a target="_blank" href="<?php echo base_url() . 'dashboard/Cpurchase/purchase_download_attachment/' . $purchase['purchase_id'] . '?file=' . $purchase['file']; ?>" class="btn btn-success btn-sm" data-toggle="tooltip" data-placement="left" title="<?php echo display('download_attachment') ?>"><i class="fa fa-download" aria-hidden="true"></i></a>
+                                                        <?php endif; ?>
+                                                        <a href="<?php echo base_url() . 'dashboard/Cpurchase/purchase_inserted_data/' . $purchase['purchase_id']; ?>" class="btn btn-success btn-sm" data-toggle="tooltip" data-placement="left" title="<?php echo display('invoice') ?>"><i class="fa fa-window-restore" aria-hidden="true"></i></a>
+                                                        <a href="<?php echo base_url() . 'dashboard/Cpurchase_return/purchase_return_form/' . $purchase['purchase_id']; ?>" class="btn btn-warning btn-sm" data-toggle="tooltip" data-placement="left" title="<?php echo display('return_purchase') ?>"><i class="fa fa-undo" aria-hidden="true"></i></a>
+                                                        <?php if ($this->permission->check_label('manage_purchase')->update()->access()) { ?>
+                                                            <!--<a href="<?php echo base_url() . 'dashboard/Cpurchase/purchase_update_form/' . $purchase['purchase_id']; ?>"
                                                         class="btn btn-info btn-sm" data-toggle="tooltip"
                                                         data-placement="left" title="<?php echo display('update') ?>"><i
                                                             class="fa fa-pencil" aria-hidden="true"></i></a>-->
-                                            <?php } ?>
+                                                        <?php } ?>
 
-                                            <?php if ($this->permission->check_label('manage_purchase')->delete()->access()) { ?>
-                                                <a href="<?php echo base_url('dashboard/Cpurchase/purchase_delete/' . $purchase['purchase_id']) ?>"
-                                                   class="btn btn-danger btn-sm"
-                                                   onclick="return confirm('<?php echo display('are_you_sure_want_to_delete') ?>');"
-                                                   data-toggle="tooltip" data-placement="right" title=""
-                                                   data-original-title="<?php echo display('delete') ?> "><i
-                                                        class="fa fa-trash-o" aria-hidden="true"></i></a>
-                                                <?php } ?>
+                                                        <?php if ($this->permission->check_label('manage_purchase')->delete()->access()) { ?>
+                                                            <a href="<?php echo base_url('dashboard/Cpurchase/purchase_delete/' . $purchase['purchase_id']) ?>" class="btn btn-danger btn-sm" onclick="return confirm('<?php echo display('are_you_sure_want_to_delete') ?>');" data-toggle="tooltip" data-placement="right" title="" data-original-title="<?php echo display('delete') ?> "><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+                                                        <?php } ?>
 
-                                        </center>
-                                        <?php echo form_close() ?>
-                                        </td>
-                                        </tr>
-                                        <?php
+                                                    </center>
+                                                    <?php echo form_close() ?>
+                                                </td>
+                                            </tr>
+                                    <?php
+                                        }
                                     }
-                                }
-                                ?>
+                                    ?>
                                 </tbody>
                             </table>
                         </div>
