65c3b0e76c7c128b8f98584b1e5f5b6448f5eb64
Mohamed-Hassan-M-M <mohamedhassan@cis.asu.edu.eg>
2022-12-05 23:22:59 +0200

    E P

--- application/modules/dashboard/views/product/barcode_print_page.php
+++ application/modules/dashboard/views/product/barcode_print_page.php
@@ -100,15 +100,15 @@
                                         // for ($i=0; $i < $open_quantity ; $i++) {
                                         for ($i = 0; $i < $stock; $i++) {?>
                                             <?php
-                                            $padding = '25.52px';
+                                            $padding = '11.52px';
                                             if($i == 0){
-                                                $padding = '14px';
+                                                $padding = '0px';
                                             }
                                             ?>
                                             <tr>
-                                            <td class="td_text" style="display: block;border:0">
+                                            <td valign="middle" class="td_text" style="display: block;border:0;height: 95px">
                                                 <div class="barcode-inner barcode_div"
-                                                     style="width: 150px; text-align: center;height: 95px;margin:auto;padding-top: <?php echo $padding; ?> ;">
+                                                     style="width: 150px; text-align: center;margin:auto;padding-top: <?php echo $padding; ?> ;">
                                                     <div class="product-name barcode_cominfo" style="margin: 0px;">
                                                         <table style="width: 100%;text-align: center;">
                                                             <tbody>
